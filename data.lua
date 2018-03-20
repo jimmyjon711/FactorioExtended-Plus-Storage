94af0c414f8065601cd724feb0c812efeb676dcb
jimmyjon711 <jcmadill1@gmail.com>
2018-03-19 18:31:47 -0700

    Fixes #2, removed code that blindly sets collision box on all containers to a 1x1 entity

--- data.lua
+++ data.lua
@@ -3,16 +3,3 @@ require("prototypes.item.item-groups")
 require("prototypes.recipe.recipe-storage")
 require("prototypes.entity.entity-storage")
 require("prototypes.technology.technology-storage")
-
-
-
-local container = data.raw["container"]
-local logicContainer = data.raw["logistic-container"]
-
-for _,cont in pairs(container) do
-  cont.collision_box = {{-0.15, -0.15}, {0.15, 0.15}}
-end
-
-for _,cont in pairs(logicContainer) do
-  cont.collision_box = {{-0.15, -0.15}, {0.15, 0.15}}
-end
