644106330a91aa9cfa384861a1ffa883d492e217
Dmitry Mukhin <dm@uploadcare.com>
2019-03-04 20:34:35 +0300

    Fix mod crash on some(?) saves.

--- migrations/FactorioExtended-Storage_0.3.1.lua
+++ migrations/FactorioExtended-Storage_0.3.1.lua
@@ -2,7 +2,7 @@ for index, force in pairs(game.forces) do
   local technologies = force.technologies
   local recipes = force.recipes
 
-  if technologies["logistics-system-2"].researched then
+  if technologies["logistics-system-2"] ~= nil and technologies["logistics-system-2"].researched then
     recipes["titanium-logistic-chest-buffer"].enabled = true
     recipes["titanium-logistic-chest-buffer"].reload()
   end
