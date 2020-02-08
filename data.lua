be5b6f46155fdeb5af41fad86f575f658a9b264f
billbo99 <cactus_computers-github@yahoo.com>
2020-02-08 11:08:27 +0000

    0.4.1

--- data.lua
+++ data.lua
@@ -1,21 +1,18 @@
+require("prototypes.entity.__init__")
 require("prototypes.item.item-storage")
 require("prototypes.item.item-groups")
 require("prototypes.recipe.recipe-storage")
-require("prototypes.entity.entity-storage")
 require("prototypes.technology.technology-storage")
-
 
 -- Adding Extended chests to unlock with normal science when early logistics are enabled
 if settings.startup["early-logistics"].value then
-  
-  logisticRoboEffects   = data.raw.technology["logistic-robotics"].effects
-  logisticSystemEffects = data.raw.technology["logistic-system"].effects
+    logisticRoboEffects = data.raw.technology["logistic-robotics"].effects
+    logisticSystemEffects = data.raw.technology["logistic-system"].effects
 
-  table.insert(logisticRoboEffects, {type = "unlock-recipe",recipe = "titanium-logistic-chest-passive-provider"})
-  table.insert(logisticRoboEffects, {type = "unlock-recipe",recipe = "titanium-logistic-chest-storage"})
+    table.insert(logisticRoboEffects, {type = "unlock-recipe", recipe = "titanium-logistic-chest-passive-provider"})
+    table.insert(logisticRoboEffects, {type = "unlock-recipe", recipe = "titanium-logistic-chest-storage"})
 
-  table.insert(logisticSystemEffects, {type = "unlock-recipe",recipe = "titanium-logistic-chest-active-provider"})
-  table.insert(logisticSystemEffects, {type = "unlock-recipe",recipe = "titanium-logistic-chest-buffer"})
-  table.insert(logisticSystemEffects, {type = "unlock-recipe",recipe = "titanium-logistic-chest-requester"})
-
+    table.insert(logisticSystemEffects, {type = "unlock-recipe", recipe = "titanium-logistic-chest-active-provider"})
+    table.insert(logisticSystemEffects, {type = "unlock-recipe", recipe = "titanium-logistic-chest-buffer"})
+    table.insert(logisticSystemEffects, {type = "unlock-recipe", recipe = "titanium-logistic-chest-requester"})
 end
