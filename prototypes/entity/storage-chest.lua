78952d225be0749251b011758f4525ddcdba4059
billbo99 <cactus_computers-github@yahoo.com>
2020-11-24 12:05:55 +0000

    1.1

--- prototypes/entity/storage-chest.lua
+++ prototypes/entity/storage-chest.lua
@@ -1,12 +1,5 @@
 local vanilla_entity = data.raw["logistic-container"]["logistic-chest-storage"]
 
-if not vanilla_entity.fast_replaceable_group then
-    vanilla_entity.fast_replaceable_group = "container"
-end
-if not vanilla_entity.next_upgrade then
-    vanilla_entity.next_upgrade = "titanium-logistic-chest-storage"
-end
-
 local chest = table.deepcopy(vanilla_entity)
 chest.name = "titanium-logistic-chest-storage"
 chest.icon = "__FactorioExtended-Plus-Storage__/graphics/icons/titanium-logistic-chest-storage.png"
--- prototypes/technology/technology-storage.lua
+++ prototypes/technology/technology-storage.lua
@@ -1,104 +1,100 @@
 if settings.startup["early-logistics"].value then
-  data:extend(
-  {
-    {
-      type = "technology",
-      name = "early-logistics",
-      icon = "__base__/graphics/technology/logistic-system.png",
-      icon_size = 128,
-      prerequisites = {"construction-robotics", "titanium-processing"},
-      effects =
-      {
+    data:extend(
         {
-          type = "unlock-recipe",
-          recipe = "titanium-chest"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-passive-provider-2"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-active-provider-2"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-buffer-2"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-storage-2"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-requester-2"
+            {
+                type = "technology",
+                name = "early-logistics",
+                icon = "__base__/graphics/technology/logistic-system.png",
+                icon_size = 256,
+                prerequisites = {"construction-robotics", "titanium-processing"},
+                effects = {
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-chest"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-passive-provider-2"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-active-provider-2"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-buffer-2"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-storage-2"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-requester-2"
+                    }
+                },
+                unit = {
+                    count = 300,
+                    ingredients = {
+                        {"automation-science-pack", 1},
+                        {"logistic-science-pack", 1},
+                        {"chemical-science-pack", 1}
+                    },
+                    time = 60
+                },
+                order = "c-k-a-b"
+            }
         }
-      },
-      unit =
-      {
-        count = 300,
-        ingredients =
-        {
-          {"automation-science-pack", 1},
-          {"logistic-science-pack", 1},
-          {"chemical-science-pack", 1}
-        },
-        time = 60
-      },
-      order = "c-k-a-b",
-    }
-  })
+    )
 else
-  data:extend(
-  {
-    {
-      type = "technology",
-      name = "logistics-system-2",
-      icon = "__base__/graphics/technology/logistic-system.png",
-      icon_size = 128,
-      prerequisites = {"construction-robotics", "titanium-processing", "logistic-system"},
-      localised_name = {"technology-name.logistics-system-2"},
-      effects =
-      {
+    data:extend(
         {
-          type = "unlock-recipe",
-          recipe = "titanium-chest"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-passive-provider"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-active-provider"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-buffer"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-storage"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "titanium-logistic-chest-requester"
+            {
+                type = "technology",
+                name = "logistics-system-2",
+                icon = "__base__/graphics/technology/logistic-system.png",
+                icon_size = 256,
+                prerequisites = {"construction-robotics", "titanium-processing", "logistic-system"},
+                localised_name = {"technology-name.logistics-system-2"},
+                effects = {
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-chest"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-passive-provider"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-active-provider"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-buffer"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-storage"
+                    },
+                    {
+                        type = "unlock-recipe",
+                        recipe = "titanium-logistic-chest-requester"
+                    }
+                },
+                unit = {
+                    count = 300,
+                    ingredients = {
+                        {"automation-science-pack", 1},
+                        {"logistic-science-pack", 1},
+                        {"chemical-science-pack", 1},
+                        {"production-science-pack", 1},
+                        {"utility-science-pack", 1}
+                    },
+                    time = 60
+                },
+                order = "c-k-a-b"
+            }
         }
-      },
-      unit =
-      {
-        count = 300,
-        ingredients =
-        {
-          {"automation-science-pack", 1},
-          {"logistic-science-pack", 1},
-          {"chemical-science-pack", 1},
-          {"production-science-pack", 1},
-          {"utility-science-pack", 1}
-        },
-        time = 60
-      },
-      order = "c-k-a-b",
-    }
-  })
+    )
 end
